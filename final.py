8becb7a7cb3db9dd6677ae85896c6b09d87cfa1e
SmooveKing <133826450+SmooveKing@users.noreply.github.com>
2023-05-23 09:15:19 -0700

    Add files via upload

---
+++ final.py
@@ -0,0 +1,9 @@
+import mysql. connector
+conn = mysql.connector.connect(host='localhost', password='Sandi1997@', user='root')
+
+def __init__(self,)
+            host="localhost",
+            port="YourPort",
+            database="banks_portal",
+            user='root',
+            password='Sandi1997@')
